932b5a7ad010ffca8074f4c2e6c52bfb3af296c3
Nick HO <hochaucan@gmail.com>
2015-08-01 13:05:14 +0700

    Create customer controller

---
+++ room4u-war/src/java/Controller/CustomerController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import com.room4u.dao.CustomerFacade;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Nick
+ */
+@ManagedBean
+@SessionScoped
+public class CustomerController {
+    @EJB
+    private CustomerFacade customerFacade;
+
+    /**
+     * Creates a new instance of CustomerController
+     */
+    public CustomerController() {
+    }
+    
+}
