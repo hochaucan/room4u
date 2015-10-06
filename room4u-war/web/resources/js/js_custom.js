f1eb31ed00ef340a44d0f253889b230f8f04cb47
NickHo <NickHo@DESKTOP-NJLQ4R1>
2015-10-07 01:39:16 +0700

    Add dang phong button

--- room4u-war/web/resources/js/js_custom.js
+++ room4u-war/web/resources/js/js_custom.js
@@ -3,44 +3,34 @@
 // In the following example, markers appear when the user clicks on the map.
 // Each marker is labeled with a single alphabetical character.
 
-var markers = [];
+
 var map;
+var markers = [];
 var panorama;
 var long = -122.254811, lat = 37.869260;
+var curLong, curLat;
 function initMap() {
 
-    // Distance service
-    var bounds = new google.maps.LatLngBounds;
-    var origin1 = {lat: 55.93, lng: -3.118};
-    var origin2 = 'Greenwich, England';
-    var destinationA = 'Stockholm, Sweden';
-    var destinationB = {lat: 50.087, lng: 14.421};
-    var destinationIcon = 'https://chart.googleapis.com/chart?' +
-            'chst=d_map_pin_letter&chld=D|FF0000|000000';
-    var originIcon = 'https://chart.googleapis.com/chart?' +
-            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
-
-
-//Street view
-    var haightAshbury = {lat: 37.769, lng: -122.446};
+    // Initialize Map
     var divMap = document.getElementById('map');
-    //var divMap = $("#map");
     map = new google.maps.Map(divMap, {
         center: {lat: -34.397, lng: 150.644},
         zoom: 15
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
 
-    // Try HTML5 geolocation.
+    // Get current position
+    var infoWindow = new google.maps.InfoWindow({map: map});
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
+            curLong = position.coords.longitude;
+            curLat = position.coords.latitude;
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            infoWindow.setContent('Your current location.');
             map.setCenter(pos);
         }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
@@ -50,13 +40,15 @@ function initMap() {
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
+
     // This event listener will call addMarker() when the map is clicked.
     map.addListener('click', function (event) {
-        deleteMarkers();
-        addMarker(event.latLng);
+       // deleteMarkers();
+        //addMarker(event.latLng);
         // alert("Latitude: " + event.latLng.lat() + " " + ", longitude: " + event.latLng.lng());
         long = event.latLng.lng();
         lat = event.latLng.lat();
+        //alert(long+" " +lat)
         panorama = new google.maps.StreetViewPanorama(
                 document.getElementById('street-view'),
                 {
@@ -64,19 +56,7 @@ function initMap() {
                     pov: {heading: 165, pitch: 0},
                     zoom: 1
                 });
-
-//       panorama = new google.maps.StreetViewPanorama(
-//            document.getElementById('street-view'),
-//            {
-//                position: {lat: 37.869260, lng: -122.254811},
-//                pov: {heading: 165, pitch: 0},
-//                zoom: 1
-//            });
     });
-
-
-    // Adds a marker at the center of the map.
-    addMarker(haightAshbury);
 
 //Street View
     panorama = new google.maps.StreetViewPanorama(
@@ -87,14 +67,96 @@ function initMap() {
                 zoom: 1
             });
 
+    $("#btnSearch").click(function () {
+        var radius = $("#sltRadius").val();
+        distanceService(radius);
+
+    });
+    
+  
 
 //Distance Service
-    var geocoder = new google.maps.Geocoder;
 
+//    var bounds = new google.maps.LatLngBounds;
+//    var origin1 = {lat: 55.93, lng: -3.118};
+//    var origin2 = 'Greenwich, England';
+//    var destinationA = 'Stockholm, Sweden';
+//    var destinationB = {lat: 50.087, lng: 14.421};
+//    var destinationIcon = 'https://chart.googleapis.com/chart?' +
+//            'chst=d_map_pin_letter&chld=D|FF0000|000000';
+//    var originIcon = 'https://chart.googleapis.com/chart?' +
+//            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+//    var geocoder = new google.maps.Geocoder;
+//    var service = new google.maps.DistanceMatrixService;
+//    service.getDistanceMatrix({
+//        origins: [origin1],
+//        destinations: [destinationA],
+//        travelMode: google.maps.TravelMode.DRIVING,
+//        unitSystem: google.maps.UnitSystem.METRIC,
+//        avoidHighways: false,
+//        avoidTolls: false
+//    }, function (response, status) {
+//        if (status !== google.maps.DistanceMatrixStatus.OK) {
+//            alert('Error was: ' + status);
+//        } else {
+//            var originList = response.originAddresses;
+//            var destinationList = response.destinationAddresses;
+//            var outputDiv = document.getElementById('output');
+//            outputDiv.innerHTML = '';
+//            //deleteMarkers(markersArray);
+//
+//            var showGeocodedAddressOnMap = function (asDestination) {
+//                var icon = asDestination ? destinationIcon : originIcon;
+//                return function (results, status) {
+//                    if (status === google.maps.GeocoderStatus.OK) {
+//                        map.fitBounds(bounds.extend(results[0].geometry.location));
+//                        markers.push(new google.maps.Marker({
+//                            map: map,
+//                            position: results[0].geometry.location,
+//                            icon: icon
+//                        }));
+//                    } else {
+//                        alert('Geocode was not successful due to: ' + status);
+//                    }
+//                };
+//            };
+//
+//            for (var i = 0; i < originList.length; i++) {
+//                var results = response.rows[i].elements;
+//                geocoder.geocode({'address': originList[i]},
+//                showGeocodedAddressOnMap(false));
+//                for (var j = 0; j < results.length; j++) {
+//                    geocoder.geocode({'address': destinationList[j]},
+//                    showGeocodedAddressOnMap(true));
+//                    outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+//                            ': ' + results[j].distance.text + ' in ' +
+//                            results[j].duration.text + '<br>';
+//                }
+//            }
+//        }
+//    });
+}
+
+
+function distanceService(radius) {
+    deleteMarkers();
+    var bounds = new google.maps.LatLngBounds;
+    //var origin1 = '546A, Hung Phu, Phuong 9, Quan 8, TP Ho Chi Minh, Vietnam'//{lat: 55.93, lng: -3.118};
+    var yourLocation = {lat: curLat, lng: curLong};
+    //var origin2 = 'Greenwich, England';
+    var destinationA = 'Khu dan cu EHOME3, TP Ho Chi Minh'//'Stockholm, Sweden';
+    var destinationB = 'duong D1, quan Binh Thanh, TP Ho Chi Minh, Vietnam';
+    var destinationC = '5B Ton Duc Thang, Quan 1, TP Ho Chi Minh, Vietnam';
+    var destinationD = '546A, Hung Phu, phuong 9, quan 8, TP Ho Chi Minh, Vietnam';
+    var destinationIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=D|FF0000|000000';
+    var originIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+    var geocoder = new google.maps.Geocoder;
     var service = new google.maps.DistanceMatrixService;
     service.getDistanceMatrix({
-        origins: [origin1],
-        destinations: [destinationA],
+        origins: [yourLocation],
+        destinations: [destinationA, destinationB, destinationC, destinationD],
         travelMode: google.maps.TravelMode.DRIVING,
         unitSystem: google.maps.UnitSystem.METRIC,
         avoidHighways: false,
@@ -130,11 +192,15 @@ function initMap() {
                 geocoder.geocode({'address': originList[i]},
                 showGeocodedAddressOnMap(false));
                 for (var j = 0; j < results.length; j++) {
-                    geocoder.geocode({'address': destinationList[j]},
-                    showGeocodedAddressOnMap(true));
-                    outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
-                            ': ' + results[j].distance.text + ' in ' +
-                            results[j].duration.text + '<br>';
+
+                    // Just get room around current location with radius
+                    if (parseFloat(results[j].distance.text).toFixed(2) <= parseInt(radius)) {
+                        geocoder.geocode({'address': destinationList[j]},
+                        showGeocodedAddressOnMap(true));
+                        outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+                                ': ' + results[j].distance.text + ' in ' +
+                                results[j].duration.text + '<br>';
+                    }
                 }
             }
         }
