518672a8a01c0b0243e8ba0eacb3358edccac88e
NickHo <NickHo@DESKTOP-NJLQ4R1>
2015-10-10 17:06:08 +0700

    Add new entity

---
+++ room4u-war/src/java/Controller/LandlordController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author NickHo
+ */
+@ManagedBean
+@SessionScoped
+public class LandlordController {
+
+    /**
+     * Creates a new instance of LandlordController
+     */
+    public LandlordController() {
+    }
+    
+}
