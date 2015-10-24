b4619fe335eba1ff8a57357eab6a0c2b7cf7c115
JixthSune <JixthSune@Jixth>
2015-10-24 18:49:20 +0700

    Completed code Add, Update, Delete FAQ Create js_faq_cuong to validate

--- room4u-war/src/java/Controller/FAQController.java
+++ room4u-war/src/java/Controller/FAQController.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Controller;
 
 import com.room4u.dao.FaqFacade;
 import com.room4u.dao.FaqFacadeLocal;
 import com.room4u.model.Faq;
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,10 +24,21 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "faq")
 @SessionScoped
 public class FAQController {
+
     @EJB
     private FaqFacadeLocal faqFacade;
     private Faq curQ;
-    
+
+    private String faqId;
+    private String faqIdDel;
+
+    public String getFaqId() {
+        return faqId;
+    }
+
+    public void setFaqId(String faqId) {
+        this.faqId = faqId;
+    }
     private String question;
     private String answer;
 
@@ -37,38 +48,45 @@ public class FAQController {
     public FAQController() {
     }
 
-    public List<Faq> getQuestions()
-    {
+    public List<Faq> getQuestions() {
         return faqFacade.findAll();
     }
-    
-    public String addFAQ()
-    {
+
+    public String addFAQ() {
         curQ = new Faq();
         Date date = new Date();
         DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
         curQ.setId(1);
-        curQ.setQuestion(question);
-        curQ.setAnswer(answer);
+        curQ.setQuestion(question.trim());
+        curQ.setAnswer(answer.trim());
         curQ.setDate(date);
         faqFacade.create(curQ);
         return "faqAdmin";
     }
-    
-    public String updateFAQ()
-    {
-        Date d = new Date();
-        SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy/MM/dd");
-        String s=timeFormat.format(d.getTime());
-        curQ.setDate(d);
-        faqFacade.edit(curQ);
-        curQ = new Faq();
+
+    public String updateFAQ()  {
+        try {
+            Date date = new Date();
+            DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+            Faq faq = faqFacade.find(Integer.parseInt(faqId));
+            faq.setQuestion(question.trim());
+            faq.setAnswer(answer.trim());
+            faq.setDate(date);
+            faqFacade.edit(faq);
+        } catch (Exception ex) {
+            printStackTrace();
+        }
         return "faqAdmin";
     }
-    
-    public String deleteFAQ()
-    {
-        faqFacade.remove(curQ);
+
+    public String deleteFAQ() {
+        try{
+            Faq faq = faqFacade.find(Integer.parseInt(faqIdDel));
+            faqFacade.remove(faq);
+        }catch(Exception ex)
+        {
+            printStackTrace();
+        }
         return "faqAdmin";
     }
 
@@ -113,5 +131,19 @@ public class FAQController {
     public void setAnswer(String answer) {
         this.answer = answer;
     }
-    
+
+    /**
+     * @return the faqIdDel
+     */
+    public String getFaqIdDel() {
+        return faqIdDel;
+    }
+
+    /**
+     * @param faqIdDel the faqIdDel to set
+     */
+    public void setFaqIdDel(String faqIdDel) {
+        this.faqIdDel = faqIdDel;
+    }
+
 }
