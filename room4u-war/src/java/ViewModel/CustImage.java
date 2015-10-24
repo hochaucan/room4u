d046c6aca5ddf96d888d5d29d2526b02805e3261
Z <Z@SOCRATES>
2015-10-24 18:31:43 +0700

    Cap nhat cac tinh nang, Dang ky new user, Dang nhap + personal profile. UPDATE

---
+++ room4u-war/src/java/ViewModel/CustImage.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ViewModel;
+
+/**
+ *
+ * @author Z
+ */
+public class CustImage {
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
